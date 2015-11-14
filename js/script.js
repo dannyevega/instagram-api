b2ccd37fc124a820cca3f4913463d00bab325361
Danny Vega <dannyevega@gmail.com>
2015-11-13 16:01:14 -0800

    working google maps api that returns a map with the location entered. Need to implement autocomplete and pass lat and lng values to IG api to pull up relevant photos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+var $igForm = $('#ig-search');
+var $igInputQuery = $('#ig-query');
+var $instagram = $('#instagram');
+var $igHeading = $('#ig-heading');
+var $googleForm = $('#google-search');
+var $googleInputQuery = $('#google-query')
+
 var Instagram = {
 	// store the application settings 
 	config: {},
@@ -10,12 +17,12 @@ var Instagram = {
 	},
 	// Get a list of popular media
 	popular: function(callback){
-		var endpoint = this.BASE_URL + '/media/popular?client_id=' + this.config.client_id;
+		var endpoint = this.BASE_URL + '/media/popular?client_id=' + this.config.client_id + '&count=50';
 		this.getJSON(endpoint, callback);		
 	},
 	// Get a list of recently tagged media
 	hashtag: function(tag, callback){
-		var endpoint = this.BASE_URL + '/tags/' + tag + '/media/recent?client_id=' + this.config.client_id;
+		var endpoint = this.BASE_URL + '/tags/' + tag + '/media/recent?client_id=' + this.config.client_id + '&count=50';
 		this.getJSON(endpoint, callback);
 	},
 	// Takes url and callback 
@@ -37,7 +44,6 @@ Instagram.init({
 });
 
 var appendImages = (function(){
-	var $instagram = $('#instagram');
 	
 	return function(response){
 		for(var i = 0; i < response.data.length; i++){
@@ -48,22 +54,107 @@ var appendImages = (function(){
 
 })();
 
-Instagram.popular(function(response){
-	appendImages(response);
-});
+// Instagram.popular(function(response){
+// 	$igHeading.text("Currently popular on Instagram");
+// 	appendImages(response);
+// 	console.log(response)
+// });
 
-var $form = $('#search')
-var $inputQuery = $('#query')
 
-$form.submit(function(el){
-	el.preventDefault();
-	var tagName = $inputQuery.val();
-	Instagram.hashtag(tagName, function(response){
-		var $instagram = $('#instagram');
-		$instagram.empty();
-			appendImages(response);
+// $igForm.submit(function(el){
+// 	el.preventDefault();
+// 	var tagName = $igInputQuery.val();
+// 	Instagram.hashtag(tagName, function(response){
+// 		$igHeading.text("Results for #" + tagName);	
+// 		$instagram.empty();
+// 		appendImages(response);
+// 		$igInputQuery.val('');
+// 		console.log(response);
+// 	});
+// });
+
+
+
+/** ***************************************************************************** **/
+
+
+
+
+// function searchResults(results, status){
+// 	console.log(results); 
+
+// 	for(var i = 0; i < results.length; i++){
+// 		var marker = new google.maps.Marker({
+// 			position: results[i].geometry.location,
+// 			map: map,
+// 			icon: results[i].icon
+// 		});	
+// 	}
+// }
+
+// MAYBE USE TO SHOW NEAR IG POSTS WITH BOUNDS 
+
+// function performSearch(){
+
+//  var request = {
+//  		bounds: map.getBounds(),
+//  		name: "McDonald's"
+//   };
+
+// 	service.nearbySearch(request, searchResults);
+// }
+
+function initialize(response){ 
+
+	var currentLocation = new google.maps.LatLng(response.results[0].geometry.location.lat, response.results[0].geometry.location.lng)
+
+	var mapOptions = {
+	  center: currentLocation,
+	  zoom: 12,
+	  mapTypeId: google.maps.MapTypeId.ROADMAP  	
+	}
+
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: currentLocation,
+		map: map,		
 	});
-});
 
+	service = new google.maps.places.PlacesService(map);	
+
+	// google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+
+	// $('#refresh').click(performSearch);
+
+}
+
+
+var Google = {	
+
+	getLocation: function(callback){
+		this.getJSON(callback)		
+	},
+
+	getJSON: function(callback){
+		var BASE_URL = 'http://maps.googleapis.com/maps/api/geocode/json?address=';		
+		var googleUserInput = $googleInputQuery.val();		
+		$.ajax({
+			type: 'GET',
+			url: BASE_URL + googleUserInput,
+			dataType: 'json',
+			success: function(response){
+				if(typeof callback === 'function') callback(response);
+			}
+		});		
+	}
+};
+
+$googleForm.submit(function(el){
+	el.preventDefault()
+	Google.getJSON(function(response){
+		initialize(response);
+	});	
+});
 
 
